e4675cfd3cc9420ba84751d6ef39ab7750a37c7a
Evevar <2529197551@qq.com>
2024-07-17 14:48:18 +0800

    完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
-import Article from "@/pages/Article";
-import Publish from "@/pages/Publish";
-import Home from "@/pages/Home";
+// import Article from "@/pages/Article";
+// import Publish from "@/pages/Publish";
+// import Home from "@/pages/Home"
 import AuthRoute from "@/components/AuthRoute";
 import { createBrowserRouter } from 'react-router-dom'
+import { Suspense, lazy } from "react";
 
+
+//lazy函数对组件进行导入
+const Home = lazy(() => import("@/pages/Home"))
+const Article = lazy(() => import("@/pages/Article"))
+const Publish = lazy(() => import("@/pages/Publish"))
 //配置路由实例
 const router = createBrowserRouter([
     {
@@ -14,15 +20,15 @@ const router = createBrowserRouter([
         children: [
             {
                 index: true,
-                element: <Home />,
+                element: <Suspense fallback={'加载中'}><Home /></Suspense>,
             },
             {
                 path: 'article',
-                element: <Article />,
+                element: <Suspense fallback={'加载中'}><Article /></Suspense>,
             },
             {
                 path: 'publish',
-                element: <Publish />,
+                element: <Suspense fallback={'加载中'}><Publish /></Suspense>,
             },
         ],
     },
